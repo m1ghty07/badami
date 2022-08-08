c3096132bfb6dd3b7e79a60e254d35299f30c858
m1ghty07 <dulchenko_s@ukr.net>
2022-08-08 16:45:55 +0300

    refactor & fix slider

--- app/js/jquery.js
+++ app/js/jquery.js
@@ -1,48 +1,34 @@
-$(function(){
-
-    $(".menu, .header").on("click", "a", function (event) {
-      
-      event.preventDefault();
-  
-      var id = $(this).attr('href'),
-  
-        top = $(id).offset().top;
-  
-      $('body,html').animate({ scrollTop: top }, 1500);
-    });
-  });
-
 $(document).ready(function () {
-    $(".gallery__items").slick({
-      infinite: true,
-      arrows: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-          },
+  $('.gallery__items').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
         },
-        {
-          breakpoint: 768,
-          settings: {
-            infinite: true,
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 640,
-          settings: {
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    });
+      },
+    ],
   });
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,102 +1,92 @@
-const btn = document.querySelector(".menu__burger"),
-  menuList = document.querySelector(".menu__list");
+// Burger
+const btn = document.querySelector('.menu__burger'),
+  menuList = document.querySelector('.menu__list');
 
-btn.addEventListener("click", () => {
-  menuList.classList.toggle("active");
+btn.addEventListener('click', () => {
+  menuList.classList.toggle('active');
   document.body.classList.toggle('lock');
 });
 
-
-document.querySelectorAll(".tabs__wrapper").forEach((e) => {
-  const tabs = e.querySelectorAll(".tabs__item"),
-    tabsContent = e.querySelectorAll(".tabs__block"),
-    tabsParent = e.querySelector(".tabs__items");
+// Tabs
+document.querySelectorAll('.tabs__wrapper').forEach((e) => {
+  const tabs = e.querySelectorAll('.tabs__item'),
+    tabsContent = e.querySelectorAll('.tabs__block'),
+    tabsParent = e.querySelector('.tabs__items');
 
   function hideContent() {
     tabsContent.forEach((item) => {
-      item.style.display = "none";
+      item.style.display = 'none';
     });
     tabs.forEach((item) => {
-      item.classList.remove("tabs__item--active");
+      item.classList.remove('tabs__item--active');
     });
   }
 
   function tabDisplay(i) {
-    tabsContent[i].style.display = "block";
-    tabs[i].classList.add("tabs__item--active");
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabs__item--active');
   }
   hideContent();
   tabDisplay(0);
 
-  tabsParent.addEventListener("click", (e) => {
+  tabsParent.addEventListener('click', (e) => {
     const target = e.target;
 
-    if (target && target.classList.contains("tabs__item")) {
+    if (target && target.classList.contains('tabs__item')) {
       tabs.forEach((item, i) => {
         if (target == item) {
           hideContent();
           tabDisplay(i);
-          $(".gallery__items").slick("setPosition");
+          $('.gallery__items').slick('setPosition');
         }
       });
     }
   });
 });
 
-const modalTrigger = document.querySelectorAll("[data-modal]"),
-  modal = document.querySelector(".modal");
+// Modal
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+  modal = document.querySelector('.modal');
 
 function openModal() {
-  modal.classList.add("show");
-  modal.classList.remove("hide");
-  document.body.style.overflow = "hidden";
+  modal.classList.add('show');
+  modal.classList.remove('hide');
+  document.body.style.overflow = 'hidden';
   clearInterval(modalTimerId);
 }
 
 modalTrigger.forEach((btn) => {
-  btn.addEventListener("click", openModal);
+  btn.addEventListener('click', openModal);
 });
 
 function closeModal() {
-  modal.classList.add("hide");
-  modal.classList.remove("show");
-  document.body.style.overflow = "";
-  window.removeEventListener("scroll", showModalByScroll);
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  document.body.style.overflow = '';
+  window.removeEventListener('scroll', showModalByScroll);
 }
 
-modal.addEventListener("click", (e) => {
-  if (e.target === modal || e.target.getAttribute("data-close") == "") {
+modal.addEventListener('click', (e) => {
+  if (e.target === modal || e.target.getAttribute('data-close') == '') {
     closeModal();
   }
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.code === "Escape" && modal.classList.contains("show")) {
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape' && modal.classList.contains('show')) {
     closeModal();
   }
 });
 
-const modalTimerId = setInterval(openModal, 50000);
+// Modal by timer & scroll
+// const modalTimerId = setInterval(openModal, 50000);
 
-function showModalByScroll() {
-  if (
-    window.pageYOffset + document.documentElement.clientHeight ===
-    document.documentElement.scrollHeight
-  ) {
-    openModal();
-  }
-}
-window.addEventListener("scroll", showModalByScroll);
-
-
- 
-
-
-
-
-
-
-
-
-
-
+// function showModalByScroll() {
+//   if (
+//     window.pageYOffset + document.documentElement.clientHeight ===
+//     document.documentElement.scrollHeight
+//   ) {
+//     openModal();
+//   }
+// }
+// window.addEventListener('scroll', showModalByScroll);
