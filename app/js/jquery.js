b6dcfc3f87160a5fd88cae95776a551427665824
m1ghty07 <dulchenko_s@ukr.net>
2022-08-21 18:11:10 +0300

    add burger + two section mobile

--- app/js/jquery.js
+++ app/js/jquery.js
@@ -1,40 +1,34 @@
-// $(document).ready(function () {
-//   $('.gallery__items').slick({
-//     infinite: true,
-//     arrows: true,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 1120,
-//         settings: {
-//           slidesToShow: 3,
-//         },
-//       },
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//           infinite: true,
-//         },
-//       },
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           infinite: true,
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//         },
-//       },
-//       {
-//         breakpoint: 640,
-//         settings: {
-//           infinite: true,
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//         },
-//       },
-//     ],
-//   })
-// })
+$(document).ready(function () {
+  $('.gallery__items').slick({
+    infinite: true,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,10 @@
-$(document).ready(function () {
-  $('.gallery__items').slick({
-    infinite: true,
-    arrows: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1120,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          infinite: true,
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  })
-})
-
 // Burger
 const btn = document.querySelector('.menu__burger'),
   menuList = document.querySelector('.menu__list')
 
 btn.addEventListener('click', () => {
   menuList.classList.toggle('active')
+  btn.classList.toggle('active')
   document.body.classList.toggle('lock')
 })
 
